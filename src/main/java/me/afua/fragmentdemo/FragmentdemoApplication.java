477ae619eecc3823c7bd34e9ac9b5822b5100459
aoa4eva <hi@aoa4eva.me>
2018-04-24 08:28:24 -0400

    Initial commit

---
+++ src/main/java/me/afua/fragmentdemo/FragmentdemoApplication.java
@@ -0,0 +1,12 @@
+package me.afua.fragmentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FragmentdemoApplication.class, args);
+    }
+}
